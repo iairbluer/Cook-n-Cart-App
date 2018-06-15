c29edc2adb5b85a3c2eba11880f365a739d4795d
reggmack <thegraphixninja@gmail.com>
2018-06-15 00:04:52 -0500

    Work on Shopping List Component
    Work on Shopping List Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a Test', 'http://www.trandynow.com/wp-content/uploads/2018/04/recipe.jpg'),
     new Recipe('Test Recipe', 'This is a Test', 'http://www.trandynow.com/wp-content/uploads/2018/04/recipe.jpg')
   ];
 
